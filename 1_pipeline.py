7bff610a77510294b6d79eedac25977d789f942a
Hemendra Jawariya <hemendrajawariya@gmail.com>
2025-09-27 14:42:07 +0530

    Update 1_pipeline.py

--- 1_pipeline.py
+++ 1_pipeline.py
@@ -35,5 +35,6 @@ def index():
     return render_template('1_pipeline.html', sent=sent, gen=gen, trans=trans, ner=ner, summ=summ)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 4000))
     app.run(debug=False, host='0.0.0.0', port=port)
+
