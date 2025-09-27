9bbdff2d21dcfec11d7b11248dd8e5c5493a16f9
Hemendra Jawariya <hemendrajawariya@gmail.com>
2025-09-27 13:46:22 +0530

    Update 1_pipeline.py

--- 1_pipeline.py
+++ 1_pipeline.py
@@ -12,30 +12,23 @@ def index():
         user_input = request.form.get('user_input')
         task = request.form.get('task')
 
-        # Load only the selected pipeline to reduce memory usage
         if task == 'sentiment':
-            sentiment_pipeline = pipeline('sentiment-analysis')
-            sent = sentiment_pipeline(user_input)
+            sent = pipeline('sentiment-analysis')(user_input)
 
         elif task == 'generation':
-            generation_pipeline = pipeline('text-generation')
-            gen = generation_pipeline(user_input)
+            gen = pipeline('text-generation')(user_input)
 
         elif task == 'translation':
-            translation_pipeline = pipeline('translation', model="Helsinki-NLP/opus-mt-fr-en")
-            trans = translation_pipeline(user_input)
+            trans = pipeline('translation', model="Helsinki-NLP/opus-mt-fr-en")(user_input)
 
         elif task == 'summarization':
-            summarization_pipeline = pipeline('summarization')
-            summ = summarization_pipeline(user_input)
+            summ = pipeline('summarization')(user_input)
 
         elif task == 'named_entity_recognition':
-            NER_pipeline = pipeline("ner", grouped_entities=True)
-            ner = NER_pipeline(user_input)
+            ner = pipeline("ner", grouped_entities=True)(user_input)
 
     return render_template('1_pipeline.html', sent=sent, gen=gen, trans=trans, ner=ner, summ=summ)
 
-# Bind to the port Render provides
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=False, host='0.0.0.0', port=port)
